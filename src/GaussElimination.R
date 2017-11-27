e1cc4a9b2ee4212de0829533014f15e49d8d99ed
Lukáš <lukaszahradnik96@seznam.cz>
2017-11-27 21:29:11 +0100

    Add returning solution of GaussElimination

--- src/GaussElimination.R
+++ src/GaussElimination.R
@@ -6,7 +6,7 @@ GaussElimination <- function(A, b) {
     #   b: The vector of right side.
     #
     # Returns:
-    #   The solution for given A and b.
+    #   The vector of solution for given A and b.
     
     n <- length(b)
     
@@ -14,7 +14,15 @@ GaussElimination <- function(A, b) {
         for(j in (i + 1):n) {
             k <- -A[j, i]/A[i, i]
             b[j] <- b[i] * k + b[j]
-            A[j,] <- A[i,] * k + A[j,]
+            A[j, (i:n)] <- A[i, (i:n)] * k + A[j, (i:n)]
         }
     }
+    
+    x <- b
+    x[n] <- b[n]/A[n,n]
+    for(i in (n-1):1) {
+        x[i] <- (b[i] - sum(A[i, (i+1):n] * x[(i+1):n]))/A[i,i]
+    }
+    
+    return(x)
 }
