385f6b3d7460313a8ff45749178cf74948752145
Lukáš <lukaszahradnik96@seznam.cz>
2017-12-17 00:32:22 +0100

    Add Fixed Point Method

---
+++ src/FixedPoint.R
@@ -0,0 +1,19 @@
+FixedPoint <- function(f, a, tolerance = 10e-10) {
+    #
+    #
+    # Args:
+    #   f: The function
+    #   a: The starting x coord
+    #   tolerance: Treshold to stop
+    #
+    # Returns:
+    #   The root of the function f
+    
+    while(TRUE) {
+        x <- f(a)
+        if(abs(x-a) < tolerance) break
+        a <- x
+    }
+    
+    return(x)
+}
