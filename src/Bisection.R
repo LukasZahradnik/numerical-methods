9b1ef1162b860ecafc7a729323fbbb9d45795a51
Lukáš <lukaszahradnik96@seznam.cz>
2017-12-16 01:47:43 +0100

    Add Bisection

---
+++ src/Bisection.R
@@ -0,0 +1,19 @@
+Bisection <- function(f, a, b) {
+    #
+    #
+    # Args:
+    #   f: The function
+    #   a: The left bound
+    #   b: The right bound
+    #
+    # Returns:
+    #   The root of function
+    
+    if(f(a) * f(b) < 0) {
+        while(TRUE) {
+            c <- (a + b) / 2
+            if(c == a || c == b) return(c)
+            ifelse(f(a) * f(c) < 0, b <- c, a <- c)
+        }
+    }
+}
