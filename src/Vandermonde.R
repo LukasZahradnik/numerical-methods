7f5136c270124fc956594c9b682331fe16e52992
Lukáš <lukaszahradnik96@seznam.cz>
2017-12-16 01:36:22 +0100

    Add Vandermonde

---
+++ src/Vandermonde.R
@@ -0,0 +1,20 @@
+Vandermonde <- function(x, y) {
+    #
+    #
+    # Args:
+    #   x: The x coords.
+    #   y: The y coords.
+    #
+    # Returns:
+    #   The function of interpolation
+    
+    
+    n <- length(x)
+    A <- matrix(1,n,n)
+    
+    for(i in 2:n) {
+        A[,i] <- A[,i-1] * x
+    }
+    
+    return(solve(A, y))
+}
