b80259c62212b841a340280c333d4fe47be79bd7
Lukáš <lukaszahradnik96@seznam.cz>
2017-12-17 00:14:13 +0100

    Add Regula Falsi Method

---
+++ src/RegulaFalsi.R
@@ -0,0 +1,22 @@
+RegulaFalsi <- function(f, a, b) {
+    #
+    #
+    # Args:
+    #   f: The function
+    #   a: The left bound
+    #   b: The right bound
+    #
+    # Returns:
+    #   The root of the function f
+    
+    
+    if(f(a) * f(b) < 0) {
+        while(TRUE) {
+            c <- a - f(a) * (b - a) / (f(b) - f(a))
+            if(c == a || c == b) return(c)
+            ifelse(f(a) * f(c) < 0, b <- c, a <- c)
+        }
+    }
+    
+    return(FALSE)
+}
